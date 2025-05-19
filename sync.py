8d8f2256d7cb9b90e8e97a788bd1f12b3c640957
Iñaki Úcar <iucar@fedoraproject.org>
2025-05-19 11:23:02 +0200

    sync fix

--- sync.py
+++ sync.py
@@ -37,7 +37,7 @@ def create_issue(pkg, version):
 
 def update_spec(pkg, version):
     cmd_ver = f'sed -i -E "s/(^Version:\\s*).*/\\1{version}/" {pkg}/{pkg}.spec'
-    cmd_rel = f'sed -i "/^Release:/c\Release:        1%{{?dist}}" {pkg}/{pkg}.spec'
+    cmd_rel = f'sed -i "/^Release:/c Release:        1%{{?dist}}" {pkg}/{pkg}.spec'
     cmd_upd = f'(cd {pkg} && test -f update.sh && ./update.sh || true)'
     check_output(cmd_ver, shell=True)
     check_output(cmd_rel, shell=True)
